1ceedb003321dc424579f56c65f95126e0b4be6d
GiuliaIennaco <51454621+GiuliaIennaco@users.noreply.github.com>
2021-02-27 23:53:26 -0300

    Add files via upload

--- Cria_Base.sql
+++ Cria_Base.sql
@@ -1,217 +1,294 @@
-CREATE SCHEMA trabalho_concessionaria
-
-DROP TABLE  IF EXISTS pessoa;
-DROP TABLE  IF EXISTS cliente;
-DROP TABLE  IF EXISTS funcionario;
-DROP TABLE  IF EXISTS vendedor;
 
 
-CREATE TABLE trabalho_concessionaria.pessoa (
-  id int NOT NULL ,
-  nome varchar(255) default NULL,
-  email varchar(255) default NULL,
-  telefone varchar(100) default NULL,
-  rua varchar(255) default NULL,
-  cidade varchar(255),
-  estado varchar(50) default NULL,
-  cep varchar(10) default NULL,
-  cpf_cnpj varchar(14) default NULL,
-  PRIMARY KEY (id)
-) ;
-
-CREATE TABLE trabalho_concessionaria.cliente
-(
-    id int NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES trabalho_concessionaria.pessoa(id) 
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-);
-
-CREATE TABLE trabalho_concessionaria.funcionario
-(
-    id int NOT NULL,
-    conta varchar(6) NOT NULL,
-    agencia varchar(4) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES trabalho_concessionaria.pessoa(id) 
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-);
-
-CREATE TABLE trabalho_concessionaria.gerente
-(
-    id int NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES trabalho_concessionaria.funcionario(id) 
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-);
-
-CREATE TABLE trabalho_concessionaria.rh
-(
-    id int NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES trabalho_concessionaria.funcionario(id) 
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-);
-
-CREATE TABLE trabalho_concessionaria.vendedor
-(
-    id int NOT NULL,
-    qtdvendas int default 0,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES trabalho_concessionaria.funcionario(id) 
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-);
-
-CREATE TABLE trabalho_concessionaria.comprador
-(
-    id int NOT NULL,
-    qtdcompras int default 0,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES trabalho_concessionaria.funcionario(id) 
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-);
-
-CREATE TABLE trabalho_concessionaria.nota_fiscal
-(
-    id int NOT NULL ,
-    data_emissao date,
-    valor float,
-    parcelas int,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE trabalho_concessionaria.nota_fiscal_aquisicao
-(
-    id int NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES trabalho_concessionaria.nota_fiscal(id) 
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-);
-
-CREATE TABLE trabalho_concessionaria.nota_fiscal_venda
-(
-    id int NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES trabalho_concessionaria.nota_fiscal(id) 
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-);
+INSERT INTO trabalho_concessionaria.pessoa (id, nome, email, telefone, rua, estado, cidade, cep, cpf_cnpj) VALUES
+	(1, 'Leandro da Paz', 'tmoura@bol.com.br', '(081) 2453-5544', 'Chácara Fernandes, 56', 'Ceará', 'da Conceição', '75047-441', '40385269170'),
+	(2, 'Lorena Rodrigues', 'pfogaca@araujo.org', '(051) 3063-4495', 'Viaduto Ana Sophia Caldeira, 785', 'Sergipe', 'Nascimento do Sul', '46261-013', '64015738217'),
+	(3, 'Guilherme Moraes', 'luiz-fernando65@bol.com.br', '71 1614-3186', 'Via Kamilly Santos, 1', 'Rio de Janeiro', 'Oliveira', '70497-610', '73506124862'),
+	(4, 'João Gabriel Rocha', 'guilhermelopes@gmail.com', '+55 (081) 8023 0886', 'Estrada Almeida, 79', 'Pará', 'da Conceição da Praia', '99626433', '07319542625'),
+	(5, 'Luana Martins', 'theo09@hotmail.com', '21 7072-6151', 'Aeroporto Cardoso, 3', 'Pernambuco', 'Correia da Serra', '60295-438', '18593047610'),
+	(6, 'Benjamin Barbosa', 'xcarvalho@vieira.com', '(071) 8633-0121', 'Esplanada Esther Gonçalves, 16', 'Rio de Janeiro', 'Martins de Minas', '40004-819', '81975320441'),
+	(7, 'Isadora Mendes', 'qgomes@gmail.com', '+55 41 8525 0108', 'Área Melo, 22', 'Rondônia', 'Teixeira de Minas', '31529117', '69435278000'),
+	(8, 'Bruno da Cruz', 'piresguilherme@ig.com.br', '(061) 7481 8468', 'Via Teixeira, 24', 'Roraima', 'Melo', '83169-779', '02483761526'),
+	(9, 'Evelyn Cardoso', 'pedro-henriquejesus@goncalves.com', '61 7590-7966', 'Largo de Gonçalves, 377', 'Maranhão', 'da Cruz', '68388012', '62907543865'),
+	(10, 'Laís Duarte', 'valentina58@sales.com', '+55 51 8766 3140', 'Distrito Aragão', 'Alagoas', 'Silveira do Galho', '52172-018', '85391042704'),
+	(11, 'Sra. Isis Castro', 'ecarvalho@viana.com', '(011) 6332-6753', 'Feira Fogaça, 35', 'Rio Grande do Norte', 'Pinto de Araújo', '27700-402', '24867513008'),
+	(12, 'Dr. Leandro Caldeira', 'yrodrigues@rocha.com', '+55 31 9700-6837', 'Pátio Nunes, 598', 'Rio Grande do Norte', 'Correia', '57435-832', '68359271437'),
+	(13, 'Arthur Novaes', 'gustavo-henrique89@yahoo.com.br', '51 4682 1587', 'Viaduto Julia Vieira, 8', 'Ceará', 'Moraes do Galho', '37992-568', '39612075425'),
+	(14, 'Kaique Cardoso', 'xlima@caldeira.br', '61 1774 6818', 'Sítio Viana, 82', 'Piauí', 'Azevedo', '95247-761', '98507216420'),
+	(15, 'Júlia Lima', 'carvalhorebeca@bol.com.br', '31 1301-1697', 'Trevo Barbosa, 89', 'Goiás', 'Caldeira Paulista', '50585893', '86013945748'),
+	(16, 'Letícia Rodrigues', 'valentina14@pires.br', '(081) 9965 4288', 'Parque Campos, 3', 'Distrito Federal', 'Oliveira de Peixoto', '42358-345', '60527413925'),
+	(17, 'Mariana Gonçalves', 'fcardoso@yahoo.com.br', '81 5768 5756', 'Rua de da Cunha, 25', 'Roraima', 'Farias', '57738231', '68059712411'),
+	(18, 'Enzo Rocha', 'lucas-gabriel30@das.org', '+55 (031) 0716-1365', 'Rodovia Viana', 'Rio Grande do Norte', 'Pinto', '07770-629', '70956382410'),
+	(19, 'Dr. Enzo Gabriel Monteiro', 'fariascaroline@yahoo.com.br', '41 2225-3078', 'Travessa Ramos, 131', 'Alagoas', 'Carvalho da Serra', '99488868', '92483570674'),
+	(20, 'Thales Rodrigues', 'ncavalcanti@bol.com.br', '+55 (071) 2540 5667', 'Distrito de Pires, 18', 'Sergipe', 'Ramos da Mata', '10993254', '58907234647'),
+	(21, 'João Lucas Rocha', 'maria-juliamartins@vieira.net', '+55 (051) 6635 5127', 'Pátio Monteiro, 16', 'Piauí', 'da Cunha da Praia', '11966-780', '02794368150'),
+	(22, 'Ian Correia', 'isabelmelo@cardoso.net', '41 1147 0350', 'Rua Ramos, 4', 'Distrito Federal', 'Souza da Mata', '76940-005', '49063125780'),
+	(23, 'Sra. Lorena da Luz', 'fogacaevelyn@hotmail.com', '+55 81 9886-5640', 'Estrada Olivia Moura, 904', 'Rio Grande do Sul', 'da Mota', '53495-924', '69045327856'),
+	(24, 'Dra. Lívia Ribeiro', 'imelo@bol.com.br', '+55 (031) 4245-1222', 'Estrada Duarte, 509', 'Rio Grande do Norte', 'Azevedo', '37255-982', '35609872103'),
+	(25, 'Bruna da Costa', 'vmendes@ig.com.br', '84 4765 8635', 'Rua de Monteiro, 362', 'Pernambuco', 'Nogueira', '31523642', '72391608403'),
+	(26, 'Maria Cecília Sales', 'ymoraes@yahoo.com.br', '+55 (031) 4726 7153', 'Largo de Pires, 33', 'Espírito Santo', 'Costela do Oeste', '08498-687', '05782391677'),
+	(27, 'Vicente Gomes', 'daniela69@bol.com.br', '11 7448 2135', 'Jardim Jesus, 7', 'Ceará', 'Costa Paulista', '80092202', '18659704258'),
+	(28, 'Catarina Moraes', 'da-costamarcelo@castro.com', '(061) 1859 0097', 'Lago Cavalcanti', 'Bahia', 'Monteiro', '58161251', '07934265883'),
+	(29, 'Isabella Ribeiro', 'stephany87@bol.com.br', '(051) 2484-4155', 'Esplanada Sales, 718', 'Espírito Santo', 'Cavalcanti', '56292363', '49768012331'),
+	(30, 'Bruno Gomes', 'lara78@hotmail.com', '+55 (021) 1703 5321', 'Vila de Nascimento, 38', 'Rondônia', 'Lopes de Minas', '29885-580', '67314802580'),
+	(31, 'Luigi Ramos', 'raulcostela@viana.net', '31 6695 9602', 'Área Kaique Costa, 94', 'Mato Grosso do Sul', 'Costa do Sul', '03016275', '56012783426'),
+	(32, 'João Felipe Dias', 'pietrosales@gmail.com', '(051) 2064-7968', 'Quadra de das Neves, 8', 'Espírito Santo', 'Azevedo da Serra', '91302414', '07163842996'),
+	(33, 'Sr. Rafael Cunha', 'augusto92@nunes.br', '84 5423-6717', 'Estação de Caldeira, 841', 'Alagoas', 'Melo', '01621-217', '69315472837'),
+	(34, 'Esther Ferreira', 'barbara37@hotmail.com', '(061) 4725 7508', 'Viela Laura Dias, 288', 'Mato Grosso do Sul', 'Souza Verde', '23111299', '85294367173'),
+	(35, 'João Vitor Cavalcanti', 'noliveira@yahoo.com.br', '(084) 6965-5663', 'Colônia Maria Fernanda Moreira, 63', 'Amapá', 'Nogueira', '89573-506', '26315794025'),
+	(36, 'Luigi Caldeira', 'nfarias@uol.com.br', '(061) 0708-4638', 'Aeroporto de Souza, 58', 'Bahia', 'da Mota', '13907-324', '53167049839'),
+	(37, 'Isabella Vieira', 'tfernandes@ig.com.br', '(051) 4381 4109', 'Jardim Laura Gonçalves, 22', 'Mato Grosso do Sul', 'Silva Verde', '04517250', '72980513695'),
+	(38, 'Lara da Paz', 'fernando90@cunha.com', '+55 (011) 5446 8326', 'Pátio Otávio Lima, 25', 'São Paulo', 'Viana de Porto', '13171507', '94607135801'),
+	(39, 'Sr. Davi Luiz Cavalcanti', 'claricemonteiro@costela.br', '+55 (041) 1432 4557', 'Campo Almeida, 11', 'Bahia', 'Sales de Minas', '99465636', '04359627874'),
+	(40, 'Laura da Paz', 'marcelalima@da.org', '+55 31 1503-0049', 'Residencial da Cunha, 18', 'Mato Grosso do Sul', 'Lima de Oliveira', '50093668', '51476028362');
 
 
-CREATE TABLE trabalho_concessionaria.venda
-(
-    id int NOT NULL ,
-    quantidade int default 0,
-    cliente int NOT NULL,
-    vendedor int NOT NULL,
-    nota_fiscal int NOT NULL,
-    desconto float default 0,
-  	PRIMARY KEY (id),
-    FOREIGN KEY (cliente) REFERENCES trabalho_concessionaria.cliente(id) 
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
-    FOREIGN KEY (vendedor) REFERENCES trabalho_concessionaria.vendedor(id) 
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
-    FOREIGN KEY (nota_fiscal) REFERENCES trabalho_concessionaria.nota_fiscal_venda(id) 
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-) ;
+INSERT INTO trabalho_concessionaria.funcionario (id, conta, agencia) VALUES
+    (1, 1022737-7, 1467),
+    (2, 0432483-8, 0519),
+    (3, 43792159-1, 3369),
+    (4, 0737776-2, 0410),
+    (5, 1148675-4, 1669),
+    (6, 50551576-6, 4251),
+    (7, 1128836-1, 2382),
+    (8, 295078-2, 2632),
+    (9, 1099124-7, 4624),
+    (10, 1104564-7, 3846),
+    (11, 33091-4, 6326),
+    (12, 1181986-9, 2473),
+    (13, 1337197-0, 0511),
+    (14, 173457-1, 4652),
+    (15, 53030362-6, 1627),
+    (16, 73880-2, 3215),
+    (17, 1145037-1, 0367),
+    (18, 199800-5, 3386),
+    (19, 0600532-2, 2505),
+    (20, 05492-4, 8854);
 
-CREATE TABLE trabalho_concessionaria.aquisicao
-(
-    id int NOT NULL ,
-    quantidade int default 0,
-    comprador int NOT NULL,
-    nota_fiscal int NOT NULL,
-  	PRIMARY KEY (id),
-    FOREIGN KEY (comprador) REFERENCES trabalho_concessionaria.comprador(id) 
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
-    FOREIGN KEY (nota_fiscal) REFERENCES trabalho_concessionaria.nota_fiscal_aquisicao(id) 
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-) ;
+INSERT INTO trabalho_concessionaria.vendedor (id, qtdvendas) VALUES
+	(10, 5),
+	(6, 6),
+	(5, 3),
+	(3, 7),
+	(9, 9),
+	(13, 5),
+	(4, 10),
+	(20, 4),
+	(1, 5),
+	(15, 9),
+	(8, 6),
+	(12, 9),
+	(16, 3),
+	(18, 1),
+	(17, 8),
+	(7, 8),
+	(19, 2),
+	(2, 9),
+	(11, 1),
+	(14, 2);
+
+INSERT INTO trabalho_concessionaria.cliente (id) VALUES
+	(21),
+	(22),
+	(23),
+	(24),
+	(25),
+	(26),
+	(27),
+	(28),
+	(29),
+	(30),
+	(31),
+	(32),
+	(33),
+	(34),
+	(35),
+	(36),
+	(37),
+	(38),
+	(39),
+	(40);
+
+INSERT INTO trabalho_concessionaria.gerente (id) VALUES
+	(2),
+	(1),
+	(15),
+	(9),
+	(20),
+	(6),
+	(13),
+	(19),
+	(5),
+	(11),
+	(10),
+	(8),
+	(16),
+	(17),
+	(3),
+	(12),
+	(18),
+	(14),
+	(4);
+
+INSERT INTO trabalho_concessionaria.rh (id) VALUES
+	(13),
+	(1),
+	(4),
+	(2),
+	(3),
+	(6),
+	(7),
+	(14),
+	(18),
+	(10),
+	(5),
+	(12),
+	(11),
+	(8),
+	(9),
+	(15),
+	(17),
+	(16),
+	(19);
+
+INSERT INTO trabalho_concessionaria.comprador (id, qtdcompras) VALUES
+	(2, 5),
+	(6, 20),
+	(5, 4),
+	(10, 2),
+	(17, 6),
+	(15, 5),
+	(3, 6),
+	(16, 8),
+	(7, 3),
+	(9, 3),
+	(12, 7),
+	(4, 8),
+	(18, 6),
+	(13, 5),
+	(1, 92),
+	(11, 8),
+	(20, 3),
+	(19, 3),
+	(8, 13),
+	(14, 6);
+
+INSERT INTO trabalho_concessionaria.nota_fiscal (id, data_emissao, valor, parcelas) VALUES
+	(1, '2002-02-20 01:36:12', 45773.90, 20),
+	(2, '1977-07-05 07:19:54', 372686.79, 10),
+	(3, '1975-07-22 17:13:46', 45738.89, 6),
+	(4, '2010-04-10 16:49:43', 98687.7, 4),
+	(5, '1995-04-03 04:02:03', 197219.37, 8),
+	(6, '1974-12-27 17:32:25', 89276.12, 10),
+	(7, '1996-10-24 23:11:26', 46738, 10),
+	(8, '2008-05-15 08:31:42', 114491.7, 24),
+	(9, '2006-06-08 08:34:00', 802827.4, 12),
+	(10, '1999-05-13 17:31:43', 49655.65, 10);
 
 
-CREATE TABLE trabalho_concessionaria.patio
-(
-    id int NOT NULL ,
-    nome varchar(255) default NULL,
-    capacidade int default 10,
-    cidade varchar(255) default NULL,
-    estado varchar(50) default NULL,
-    bairro varchar(255) default NULL,
-    cep varchar(10) default NULL,
-    PRIMARY KEY (id)
-) ;
+INSERT INTO trabalho_concessionaria.nota_fiscal_venda (id) VALUES
+	(6),
+	(4),
+	(2),
+	(7),
+	(5);
 
 
-CREATE TABLE trabalho_concessionaria.veiculo
-(
-    id int NOT NULL ,
-    quantidade int default 1,
-    ano int default NULL,
-    modelo varchar(255) default NULL,
-    cor varchar(255) default NULL,
-    tipo varchar(255) default NULL,
-    aquisicao int NOT NULL,
-    venda int default NULL,
-    patio int NOT NULL,
-  	PRIMARY KEY (id),
-    FOREIGN KEY (aquisicao) REFERENCES trabalho_concessionaria.aquisicao(id),
-    FOREIGN KEY (patio) REFERENCES trabalho_concessionaria.patio(id),
-    FOREIGN KEY (venda) REFERENCES trabalho_concessionaria.venda(id)
+INSERT INTO trabalho_concessionaria.nota_fiscal_aquisicao (id) VALUES
+	(9),
+	(8),
+	(10),
+	(4),
+	(3);
 
-) ;
 
-CREATE TABLE trabalho_concessionaria.meta_de_vendas
-(
-    id int NOT NULL ,
-    quantidade_automoveis_vendidos int default 1,
-    mes date default NULL,
-    gerente int NOT NULL,
-  	PRIMARY KEY (id),
-    FOREIGN KEY (gerente) REFERENCES trabalho_concessionaria.gerente(id)
-) ; 
+INSERT INTO trabalho_concessionaria.aquisicao (id, comprador, quantidade, nota_fiscal) VALUES
+	(1, 19, 12, 9),
+	(2, 20, 2, 10),
+	(3, 3, 8, 4),
+	(4, 15, 4, 3),
+	(5, 13, 2, 8);
 
-CREATE TABLE trabalho_concessionaria.bonificacao
-(
-    id int NOT NULL ,
-    data_bonificacao date default NULL,
-    valor float default 0,
-    gerente int NOT NULL,
-    funcionario int NOT NULL,
-  	PRIMARY KEY (id),
-    FOREIGN KEY (gerente) REFERENCES trabalho_concessionaria.gerente(id),
-    FOREIGN KEY (funcionario) REFERENCES trabalho_concessionaria.funcionario(id)
-) ;
 
-CREATE TABLE trabalho_concessionaria.pagamento
-(
-    id int NOT NULL ,
-    data_pagamento date default NULL,
-    valor float default 0,
-    rh int NOT NULL,
-    funcionario int NOT NULL,
-  	PRIMARY KEY (id),
-    FOREIGN KEY (rh) REFERENCES trabalho_concessionaria.rh(id),
-    FOREIGN KEY (funcionario) REFERENCES trabalho_concessionaria.funcionario(id)
-) ;
+INSERT INTO trabalho_concessionaria.venda (id, cliente, vendedor, nota_fiscal, quantidade, desconto) VALUES
+	(30, 21, 4, 2, 2, 0.25),
+	(25, 35, 8, 7, 7, 0.15),
+	(24, 24, 20, 4, 8, 0.2),
+	(26, 30, 15, 5, 8, 0.1);
 
-CREATE TABLE trabalho_concessionaria.gerenciamento_funcionario
-(
-    id int NOT NULL ,
-    rh int NOT NULL,
-    funcionario int NOT NULL,
-  	PRIMARY KEY (id),
-    FOREIGN KEY (rh) REFERENCES trabalho_concessionaria.rh(id),
-    FOREIGN KEY (funcionario) REFERENCES trabalho_concessionaria.funcionario(id)
-) ;
+
+INSERT INTO trabalho_concessionaria.patio (id, capacidade, cidade, estado, bairro, cep) VALUES
+	(1, 1334, 'da Mota', 'Paraíba', 'Samoa Americana', '00306780'),
+	(2, 9506, 'Lima das Pedras', 'Santa Catarina', 'Domínica', '98051-601');
+
+INSERT INTO trabalho_concessionaria.veiculo (id, quantidade, ano, modelo, cor, aquisicao, venda, patio, tipo) VALUES
+	(1, 31, 2010, 'Luigi Pereira', 'Cinza', 5, 24, 2, 'Carro'),
+	(2, 23, 2018, 'Otávio Nunes', 'Preto', 4, 24, 1, 'Moto'),
+	(3, 48, 2016, 'Eduarda Dias', 'Branco', 3, 25, 2, 'Carro'),
+	(4, 15, 2015, 'Bernardo das Neves', 'Cinza', 5, 30, 1, 'Carro'),
+	(5, 37, 2013, 'Arthur da Costa', 'Preto', 5, 30, 2, 'Moto'),
+	(6, 52, 2018, 'João Vitor das Neves', 'Preto', 4, 30, 2, 'Moto'),
+	(7, 66, 2010, 'Arthur da Cruz', 'Branco', 3, 30, 2, 'Carro'),
+	(8, 16, 2015, 'Emanuel Barbosa', 'Cinza', 3, 25, 1, 'Carro'),
+	(9, 42, 2015, 'Gabriel Correia', 'Preto', 1, 24, 2, 'Moto'),
+	(10, 41, 2013, 'Maria Clara Cavalcanti', 'Vermelho', 4, 24, 2, 'Carro'),
+	(11, 83, 2010, 'Isabelly Nogueira', 'Branco', 5, 30, 1, 'Moto'),
+	(12, 37, 2015, 'João Guilherme Rodrigues', 'Azul', 3, 24, 2, 'Carro'),
+	(13, 9, 2018, 'Juliana Gonçalves', 'Cinza', 5, 25, 2, 'Moto'),
+	(14, 68, 2015, 'Brenda da Luz', 'Azul', 2, 25, 1, 'Carro'),
+	(15, 93, 2018, 'Alexia Lopes', 'Branco', 4, 25, 1, 'Carro'),
+	(16, 53, 2010, 'Eloah da Conceição', 'Cinza', 2, 25, 1, 'Carro'),
+	(17, 21, 2013, 'Raul Mendes', 'Preto', 5, 25, 1, 'Moto'),
+	(18, 16, 2018, 'Lívia das Neves', 'Vermelho', 5, 30, 1, 'Carro'),
+	(19, 55, 2015, 'Yasmin Santos', 'Branco', 1, 30, 2, 'Moto'),
+	(20, 68, 2013, 'Igor Lima', 'Preto', 4, 24, 2, 'Moto');
+
+INSERT INTO trabalho_concessionaria.meta_de_vendas (id, quantidade_automoveis_vendidos, mes, gerente) VALUES
+	(1, 69, '2002-02-20 01:36:12', 16),
+	(2, 85, '2002-02-20 01:36:12', 9),
+	(3, 62, '2002-02-20 01:36:12', 19),
+	(4, 22, '2002-02-20 01:36:12', 17),
+	(5, 95, '2002-02-20 01:36:12', 9);
+
+
+
+INSERT INTO trabalho_concessionaria.gerenciamento_funcionario (id, rh, funcionario) VALUES
+	(4, 9, 1),
+	(10, 1, 14),
+	(8, 6, 2),
+	(9, 14, 8),
+	(3, 6, 1),
+	(7, 15, 8),
+	(6, 11, 20),
+	(1, 4, 11),
+	(5, 19, 1);
+
+INSERT INTO trabalho_concessionaria.bonificacao (id, data_bonificacao, valor, gerente, funcionario) VALUES
+	(1, '2020-08-06 02:09:32', 2359.10, 2, 17),
+	(2, '2020-11-20 00:42:09', 5032.0, 9, 17),
+	(3, '2020-07-19 23:46:23', 94771.62, 20, 10),
+	(4, '2020-11-30 03:52:36', 84193.69, 19, 13),
+	(5, '2020-12-24 17:28:39', 092040.0, 13, 1);
+
+INSERT INTO trabalho_concessionaria.pagamento (id, data_pagamento, valor, rh, funcionario) VALUES
+    (1, '2017-09-05 08:44:36',2000, 3, 14),
+    (2, '2017-11-13 08:38:41', 2000, 4, 13),
+    (3, '2017-10-16 06:02:45', 1050, 5, 12),
+    (4, '2014-04-15 22:18:27', 2000, 2, 10),
+    (5, '2013-06-18 06:33:44', 4000, 1, 9),
+    (6, '2012-12-03 00:46:21', 2000, 1, 7),
+    (7, '2016-03-21 15:35:59', 6000, 2, 8),
+    (8, '2014-04-07 23:33:57', 5000, 5, 5),
+    (9, '2017-05-08 20:40:29', 2000, 4, 6),
+    (10, '2017-01-09 07:37:58', 7000, 3, 15),
+    (11, '2017-01-13 12:02:28', 1300, 2, 16),
+    (12, '2017-04-13 12:03:57', 7300, 1, 17),
+    (13, '2017-02-17 15:31:27', 3000, 1, 20),
+    (14, '2018-01-04 01:10:30', 2315, 2, 3),
+    (15, '2018-08-16 00:55:59', 9938, 5, 1),
+    (16, '2018-02-17 12:34:29', 2345, 4, 4),
+    (17, '2018-08-25 02:23:52', 3454, 3, 2),
+    (18, '2013-02-12 15:08:39', 1233, 3, 11),
+    (19, '2013-01-22 03:28:19', 4980, 2, 18),
+    (20, '2013-04-29 21:20:21', 1980, 1, 19);
+
